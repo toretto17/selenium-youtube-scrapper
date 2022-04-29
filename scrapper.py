857ab951dc812dcb692417ef814faf482a21d1ff
Rahul Shah <74894594+toretto17@users.noreply.github.com>
2022-04-29 06:43:14 +0000

    Added new feature to send eail of the results obtained!

--- scrapper.py
+++ scrapper.py
@@ -1,8 +1,11 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import re
 import pandas as pd
+import json
+import smtplib
 
 # This is a dynamic or client side rendered page ehere it is always updated based on the new information if added/updated on that page.
 youtube_trending_url = 'https://www.youtube.com/feed/trending'
@@ -15,7 +18,6 @@ def get_driver():
   
 # Here we need to add the path of chromedriver where it is installed
   driver = webdriver.Chrome(options=chrome_options)
-  print('driverrrrrrr',driver)
   return driver
 
 def get_videos(driver):
@@ -52,6 +54,36 @@ def parse_video(video):
     'description':description
   }
 
+def send_mail(body):
+  try:
+    # server = smtplib.SMTP('smtp.gmail.com', 587)
+    # server.ehlo()
+    # server.starttls()
+    server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server_ssl.ehlo()   # optional
+    sender_mail = 'rahul.shah@bistasolutions.com'
+    receiver_mail = 'rahul.shah@bistasolutions.com'
+    sender_pswd = os.environ['gmail_password']
+    subject = 'Youtube Trending videos'
+    # body = 'Kaisan ba sab thik'
+    
+    email_text = f"""\
+    From: {sender_mail}
+    To: {receiver_mail}
+    Subject: {subject}
+
+    
+        {body}
+
+    
+    """
+    server_ssl.login(sender_mail, sender_pswd)
+    server_ssl.sendmail(sender_mail, receiver_mail, email_text)
+    server_ssl.close()
+        
+  except:
+    print('Something went wrong...')
+
 
 if __name__ == "__main__":
   print('creating driver')
@@ -61,32 +93,12 @@ if __name__ == "__main__":
   video = get_videos(driver)
   print(len(video))
 
-  # videos_data = [parse_video(x) for x in video]
-  # for i in video:
-  #   x= parse_video(i)
-  #   print(x)
   videos_data = [parse_video(i) for i in video]
-  print(videos_data)
-  print()
 
   videos_df = pd.DataFrame(videos_data)
-  print(videos_df)
 
   videos_df.to_csv('trending.csv',index=None)
 
-  # title_tag = videos.find_element(By.ID,'video-title')
-  # title = title_tag.text
-  
-  # url = title_tag.get_attribute('href')
-  
-  # thumbnail_url = videos.find_element(By.TAG_NAME,'img').get_attribute('src')
-
-  # channel_div = videos.find_element(By.CLASS_NAME,'style-scope ytd-channel-name')
-  # channel_name = channel_div.text
-
-  # video_view_posted_tag = videos.find_element(By.ID,'metadata-line').text
-  # x = re.split('views\n',video_view_posted_tag)
-  # view = x[0]
-  # posted = x[1]
-
-  # description = videos.find_element(By.ID,'description-text').text
+  print('Sendng result on an email')
+  body = json.dumps(videos_data,indent=2)
+  send_mail(body)
